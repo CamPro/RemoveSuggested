9f298254e73e2f53f7083f25550727cc6e444d11
campx <campxpro@gmail.com>
2023-03-22 22:56:36 +0700

    upgrade

--- options.js
+++ options.js
@@ -1,1 +1,30 @@
 // options.js
+
+chrome.storage.sync.get(["isRemoveSuggested", "isRemoveFbclid", "suggestedText"], function(items)
+{
+	var isRemoveSuggested = items["isRemoveSuggested"] ?? true;
+	var isRemoveFbclid = items["isRemoveFbclid"] ?? true;
+	var suggestedText = items["suggestedText"] ?? "";
+
+	$("#isRemoveSuggested").prop("checked", isRemoveSuggested);
+	$("#isRemoveFbclid").prop("checked", isRemoveFbclid);
+	$("#suggestedText").val(suggestedText);
+});
+
+$("button").click(function ()
+{
+	var isRemoveSuggested = $("#isRemoveSuggested").is(":checked");
+	var isRemoveFbclid = $("#isRemoveFbclid").is(":checked");
+	var suggestedText = $("#suggestedText").val();
+
+	//  Save data to storage across their browsers...
+	chrome.storage.sync.set({
+		"isRemoveSuggested": isRemoveSuggested,
+		"isRemoveFbclid": isRemoveFbclid,
+		"suggestedText": suggestedText
+	},
+	function()
+	{
+		console.log("saved");
+	});
+})
--- rsuggested.js
+++ rsuggested.js
@@ -2,34 +2,88 @@
 console.log("Load content script rsuggested.js");
 /*
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message)
-    return true
+	console.log(message)
+	return true
 });
 */
-var localize_text = "Gợi ý cho bạn";
+var isRemoveSuggested = true;
+var isRemoveFbclid = true;
+var suggestedText = "";
+var suggested_texts = ["Gợi ý cho bạn", "Suggested for you"];
 
-var timer = setTimeout(function () { removeSuggested(localize_text); }, 0);
+var timer = setTimeout(function () { removeFbAds() }, 0);
 let timer_sleep = 500;
+
+chrome.storage.sync.get(["removeSuggested", "removeFbclid", "suggestedText"], function(items)
+{
+	isRemoveSuggested = items["removeSuggested"] ?? true;
+	isRemoveFbclid = items["removeFbclid"] ?? true;
+	suggestedText = items["suggestedText"] ?? "";
+
+	if (suggestedText.length > 0)
+		suggested_texts.push(suggestedText);
+	//console.log(suggested_texts);
+});
 
 function nodeInsertedCallback(event)
 {
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-        removeSuggested(localize_text);
-    }, timer_sleep);
+	clearTimeout(timer);
+	timer = setTimeout(function() {
+		removeFbAds();
+	}, timer_sleep);
 };
 
+document.addEventListener("DOMNodeInserted", nodeInsertedCallback);
+
+
+
+
+// FUNCTION REMOVE ADS
+
+function removeFbAds()
+{
+	if (isRemoveSuggested)
+	{
+		//console.log("Remove suggestion");
+		suggested_texts.forEach(function (text)
+		{
+			removeSuggested(text);
+		})
+	}
+	if (isRemoveFbclid)
+	{
+		//console.log("Remove fbclid");
+		removeFbclidLinkHref();
+	}
+}
 function removeSuggested(text)
 {
-    console.log("Suggested remove ...");
-
-    let xpath = "//*[text()='" + text + "']";
-
-    let query = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    for (let i = 0, length = query.snapshotLength; i < length; ++i) {
-        var suggested = query.snapshotItem(i);
-        $(suggested).parents('div.x1lliihq, div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z').remove();
-    }
+	let xpath = "//*[text()='" + text + "']";
+	let query = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+		var suggested = query.snapshotItem(i);
+		$(suggested).parents("div.x1lliihq, div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z").remove();
+	}
 }
-
-document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+function removeFbclidLinkHref()
+{
+	document.querySelectorAll("a[href*='fbclid']").forEach(function (elem)
+	{
+		var href = decodeURIComponent(elem.href);
+		href = href.replace("https://l.facebook.com/l.php?u=", "");
+		console.log(href);
+		var url = new URL(href);
+		var params = new URLSearchParams(url.search);
+		params.delete("fbclid");
+		params.delete("h");
+		params.delete("__tn__");
+		params.delete("c[0]");
+		href = url.origin + url.pathname + "?" + params.toString();
+		// create element
+		var link = document.createElement("a");
+		link.href = href;
+		link.innerText = elem.innerText;
+		link.target = "_blank";
+		$(elem).replaceWith(link);
+	})
+}
