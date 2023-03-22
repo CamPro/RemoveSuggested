fd673d7b00dded3c7165ba4105c8b438a3911922
camtvmatech <124016404+camtvmatech@users.noreply.github.com>
2023-03-22 17:22:31 +0700

    upload code

---
+++ background.js
@@ -0,0 +1,10 @@
+// backgroud
+/*
+var onUpdatedEvent = function (tabId, changeInfo, tab) {
+    chrome.tabs.sendMessage(tabId, {action: "onPageLoaded"}, function(response) {
+        console.log(response);
+    });
+};
+
+chrome.tabs.onUpdated.addListener(onUpdatedEvent);
+*/
---
+++ rsuggested.js
@@ -0,0 +1,34 @@
+// content script
+console.log("Load content script rsuggested.js");
+/*
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message)
+    return true
+});
+*/
+var localize_text = "Gợi ý cho bạn";
+
+var timer = setTimeout(function () { removeSuggested(localize_text); }, 0);
+
+function nodeInsertedCallback(event)
+{
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+        removeSuggested(localize_text);
+    }, 1000);
+};
+
+function removeSuggested(text)
+{
+    console.log("Suggested remove ...");
+
+    let xpath = "//*[text()='" + text + "']";
+
+    let query = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        var suggested = query.snapshotItem(i);
+        $(suggested).parents('div.x1lliihq, div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z').remove();
+    }
+}
+
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
