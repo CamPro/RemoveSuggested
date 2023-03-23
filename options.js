e77af9af641aab4c6f9de8266bc071c3f47d0cde
camtvmatech <124016404+camtvmatech@users.noreply.github.com>
2023-03-23 09:25:09 +0700

    update remove ads

--- options.js
+++ options.js
@@ -1,13 +1,15 @@
 // options.js
 
-chrome.storage.sync.get(["isRemoveSuggested", "isRemoveFbclid", "suggestedText"], function(items)
+chrome.storage.sync.get(["isRemoveSuggested", "isRemoveFbclid", "isRemoveAds", "suggestedText"], function(items)
 {
 	var isRemoveSuggested = items["isRemoveSuggested"] ?? true;
 	var isRemoveFbclid = items["isRemoveFbclid"] ?? true;
+	var isRemoveAds = items["isRemoveAds"] ?? true;
 	var suggestedText = items["suggestedText"] ?? "";
 
 	$("#isRemoveSuggested").prop("checked", isRemoveSuggested);
 	$("#isRemoveFbclid").prop("checked", isRemoveFbclid);
+	$("#isRemoveAds").prop("checked", isRemoveAds);
 	$("#suggestedText").val(suggestedText);
 });
 
@@ -15,12 +17,14 @@ $("button").click(function ()
 {
 	var isRemoveSuggested = $("#isRemoveSuggested").is(":checked");
 	var isRemoveFbclid = $("#isRemoveFbclid").is(":checked");
+	var isRemoveAds = $("#isRemoveAds").is(":checked");
 	var suggestedText = $("#suggestedText").val();
 
 	//  Save data to storage across their browsers...
 	chrome.storage.sync.set({
 		"isRemoveSuggested": isRemoveSuggested,
 		"isRemoveFbclid": isRemoveFbclid,
+		"isRemoveAds": isRemoveAds,
 		"suggestedText": suggestedText
 	},
 	function()
--- rsuggested.js
+++ rsuggested.js
@@ -8,28 +8,33 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 */
 var isRemoveSuggested = true;
 var isRemoveFbclid = true;
+var isRemoveAds = true;
 var suggestedText = "";
-var suggested_texts = ["Gợi ý cho bạn", "Suggested for you", "Suggested Post"];
 
-var timer = setTimeout(function () { removeFbAds() }, 0);
+var suggestedTexts = ["Suggested for you", "Suggested Post", "Gợi ý cho bạn"];
+
+var feedSelector = "div.x1lliihq, div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z";
+
+var timer = setTimeout(function () { removeOptions() }, 0);
 let timer_sleep = 500;
 
-chrome.storage.sync.get(["removeSuggested", "removeFbclid", "suggestedText"], function(items)
+chrome.storage.sync.get(["isRemoveSuggested", "isRemoveFbclid", "isRemoveAds", "suggestedText"], function(items)
 {
-	isRemoveSuggested = items["removeSuggested"] ?? true;
-	isRemoveFbclid = items["removeFbclid"] ?? true;
+	isRemoveSuggested = items["isRemoveSuggested"] ?? true;
+	isRemoveFbclid = items["isRemoveFbclid"] ?? true;
+	isRemoveAds = items["isRemoveAds"] ?? true;
 	suggestedText = items["suggestedText"] ?? "";
 
 	if (suggestedText.length > 0)
-		suggested_texts.push(suggestedText);
-	//console.log(suggested_texts);
+		suggestedTexts.push(suggestedText);
+	//console.log(suggestedTexts);
 });
 
 function nodeInsertedCallback(event)
 {
 	clearTimeout(timer);
 	timer = setTimeout(function() {
-		removeFbAds();
+		removeOptions();
 	}, timer_sleep);
 };
 
@@ -40,20 +45,21 @@ document.addEventListener("DOMNodeInserted", nodeInsertedCallback);
 
 // FUNCTION REMOVE ADS
 
-function removeFbAds()
+function removeOptions()
 {
-	if (isRemoveSuggested)
-	{
+	if (isRemoveSuggested) {
 		//console.log("Remove suggestion");
-		suggested_texts.forEach(function (text)
+		suggestedTexts.forEach(function (text)
 		{
 			removeSuggested(text);
 		})
 	}
-	if (isRemoveFbclid)
-	{
+	if (isRemoveFbclid) {
 		//console.log("Remove fbclid");
 		removeFbclidLinkHref();
+	}
+	if (isRemoveAds) {
+		removeAds();
 	}
 }
 function removeSuggested(text)
@@ -62,7 +68,7 @@ function removeSuggested(text)
 	let query = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 	for (let i = 0, length = query.snapshotLength; i < length; ++i) {
 		var suggested = query.snapshotItem(i);
-		$(suggested).parents("div.x1lliihq, div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z").remove();
+		$(suggested).parents(feedSelector).remove();
 	}
 }
 function removeFbclidLinkHref()
@@ -71,7 +77,7 @@ function removeFbclidLinkHref()
 	{
 		var href = decodeURIComponent(elem.href);
 		href = href.replace("https://l.facebook.com/l.php?u=", "");
-		console.log(href);
+		//console.log(href);
 		var url = new URL(href);
 		var params = new URLSearchParams(url.search);
 		params.delete("fbclid");
@@ -85,5 +91,13 @@ function removeFbclidLinkHref()
 		link.innerText = elem.innerText;
 		link.target = "_blank";
 		$(elem).replaceWith(link);
+	})
+}
+function removeAds()
+{
+	document.querySelectorAll("a[href*='ads/about']").forEach(function (elem)
+	{
+		var suggested = elem;
+		$(suggested).parents(feedSelector).remove();
 	})
 }
